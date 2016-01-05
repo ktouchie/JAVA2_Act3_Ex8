9944222276054e3915d826d7b8bcadd4dfbcd57d
Karin Touchie <ktouchie@gmail.com>
2016-01-05 15:08:46 +0100

    Create class, add prompt and variable n

---
+++ pascal.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class pascal {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many lines of PASCAL'S TRIANGLE would you like to see?")
+        int n = sc.nextInt();
+        
+        
+    }
+    
+}
