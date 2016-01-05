9bece3d1689cce731567a7b7752c3640b812d71d
Karin Touchie <ktouchie@gmail.com>
2016-01-05 15:27:43 +0100

    Declare array based on n value and fill with 1s

--- pascal.java
+++ pascal.java
@@ -10,6 +10,19 @@ class pascal {
         
     }
     
+    public static int[][] triangle(int x) {
+        int[][] array = new int[x][x];          // declare array with x lines and x columns
+        int j=0;
+        for (int i=0; i<array.length; i++) {    // fill in all the 1s
+            array[i][0]=1;
+            array[i][j]=1;
+            j++
+        }
+        
+        
+        
+    }
+    
     public static void printArray(int[][] a) {
         for (int i=0; i<x; i++) {
             for (int j=0; j<x; j++) {
